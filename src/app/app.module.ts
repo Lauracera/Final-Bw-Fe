450950a7c22cf2fa1a6b164950481fd022f578fa
GiuseppeBarresi98 <145044894+GiuseppeBarresi98@users.noreply.github.com>
2024-02-29 18:49:35 +0100

    profile page e routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/Login/login/login.component';
 import { RegisterComponent } from './auth/Register/register/register.component';
 import { RouterModule, Route } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Route[] = [];
+const routes: Route[] = [{ path: 'profile', component: ProfileComponent }];
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    ProfileComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
